0142b0d42c2c03d66ca20463e4cbe4b81a4e504d
willyfh <willyfitrahendria@gmail.com>
2023-03-17 14:51:56 +0900

    add argparse

--- upscale_images_traditional.py
+++ upscale_images_traditional.py
@@ -2,41 +2,37 @@ import os
 import cv2
 from tqdm import tqdm
 
-# parameters
-scale = 2
-interpolation = "bilinear"
+import argparse
 
-# We only use these three interpolations
-interpolations = {
-                    "bilinear": cv2.INTER_LINEAR,
-                    "bicubic": cv2.INTER_CUBIC,
-                    "lanczos": cv2.INTER_LANCZOS4
-                }
+parser = argparse.ArgumentParser(description='Upscale images with traditional approaches')
+parser.add_argument('--scale', help='scaling factor', choices=[2, 4], default=2)
+parser.add_argument('--interpolation', help='interpolation method', choices=['bilinear', 'bicubic', 'lanczos'], default='bilinear')
+parser.add_argument('--dataset_path', help='path to the list of dataset directory', default='dataset/')
+parser.add_argument('--img_dir', help='directory of images', default='images')
+args = parser.parse_args()
 
-assert interpolation in interpolations
-assert scale in [2, 4]
 
-datasets = next(os.walk('.'))[1]
+datasets = next(os.walk(args.dataset_path))[1]
 
 def upscale_images(data_path):
     """ Upscale all images inside the 'data_path' (e.g. 'path_to_dataset/train/')
     """
-    img_path = os.path.join(data_path, "images")
+    img_path = os.path.join(data_path, args.img_dir)
 
     for image_name in os.listdir(img_path):
 
         img = cv2.imread(os.path.join(img_path, image_name), cv2.IMREAD_UNCHANGED)
         # print('Original Dimensions : ',img.shape)
          
-        width = int(img.shape[1] * scale)
-        height = int(img.shape[0] * scale)
+        width = int(img.shape[1] * args.scale)
+        height = int(img.shape[0] * args.scale)
         dim = (width, height)
           
         # resize image
-        resized_img = cv2.resize(img, dim, interpolation = interpolations[interpolation])
+        resized_img = cv2.resize(img, dim, interpolation = interpolations[args.interpolation])
         # print('Resized Dimensions : ',resized_img.shape)
 
-        upscaled_img_path = os.path.join(data_path, "upscaled_images_"+interpolation+"_"+str(scale)+"x")
+        upscaled_img_path = os.path.join(data_path, "upscaled_images_"+args.interpolation+"_x"+str(args.scale))
         os.makedirs(upscaled_img_path, exist_ok=True)
         cv2.imwrite(os.path.join(upscaled_img_path, image_name), resized_img)
 
